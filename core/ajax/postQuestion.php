f5e9c1b739197a4ba63b414f10b99e5589d62afe
Viet <nohackerjustdev@gmail.com>
2021-05-08 17:30:11 +0700

    fix insert in question table postQuestion.php

--- core/ajax/postQuestion.php
+++ core/ajax/postQuestion.php
@@ -7,13 +7,14 @@ $userid = login::isLoggedIn();
 
 echo $_POST["questionTitle"];
 
-if (isset($_POST["questionTitle"]) && isset($_POST["questionContent"])){
+if (isset($_POST["questionTitle"]) && isset($_POST["questionTags"]) && isset($_POST["questionContent"])){
     $questionTitle = $_POST["questionTitle"];
     $questionTags = $_POST["questionTags"];
     $questionContent = $_POST["questionContent"];
 
 $loadFromQuestion->create('question',array("user_id" => $userid, "postBy" =>$userid, "postOn" => date("Y-m-d H:i:s", time()),
-                        "title" => $questionTitle,"tags" => $questionTags,"content" => $questionContent));
+                        "title" => $questionTitle,"tags" => $questionTags,"content" => $questionContent,
+                        "voteCount" => 0, "answerCount" => 0, "isSpam" => 0, "totalSpam" => 0));
 }else {
     echo "You have to write a title or content";
 }
