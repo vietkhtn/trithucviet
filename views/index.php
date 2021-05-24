0e84a84eca6d510b5ffa57c49fe148fb252851ed
PhamTrung99 <teotu22@gmail.com>
2021-05-24 14:30:41 +0700

    add navbar

--- views/index.php
+++ views/index.php
@@ -6,20 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
-    <?php
-     $a = 2;
-    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <div class="container">
         <div class="row">
+            <!-- Image and text -->
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/image/stackoverflow.png" width="200" height="" class="d-inline-block align-top" alt="">                 
+                </a>
+            </nav>
+        </div>
+        <div class="row">
             <div class="col-12">
                 <a href="index.php">
-                    <img src="../assets/image/stackoverflow.png" class="img-logo" />
+                    
                 </a>
-                ádasdasd
+
             </div>
         </div>
     </div>
