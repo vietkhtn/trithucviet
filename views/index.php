fd11023476e019a27150cdbaee1a34794321463e
PhamTrung99 <teotu22@gmail.com>
2021-05-24 16:03:02 +0700

    update navbar

--- views/index.php
+++ views/index.php
@@ -6,26 +6,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
+    <link href="../assets/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <script src="../assets/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="../assets/bootstrap/js/popper.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
     <div class="container">
         <div class="row">
             <!-- Image and text -->
-            <nav class="navbar navbar-light bg-light">
-                <a class="navbar-brand" href="#">
-                    <img src="../assets/image/stackoverflow.png" width="200" height="" class="d-inline-block align-top" alt="">                 
-                </a>
+            <nav class="navbar navbar-light bg-light" style="padding: 0rem;">
+                <div class="container-fluid" style="justify-content: flex-start; padding-right: 0rem;padding-left: 0rem;">
+                    <div class="col-md-auto">
+                        <a class="navbar-brand" href="#">
+                            <img src="../assets/image/stackoverflow.png" width="200" height="" class="d-inline-block align-top" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-1">
+                        <button style="/* background-color: lightslategray; */border: none;color: dimgray;padding: 11px;text-align: center;/* text-decoration: none; */display: inline-block;font-size: 14px;margin: 1px px;border-radius: 34px;font-weight: 700;width: 78px;">
+                            About
+                        </button>
+                    </div>
+                    <div class="col-md-1">
+                        <button style="/* background-color: lightslategray; */border: none;color: dimgray;padding: 11px;text-align: center;/* text-decoration: none; */display: inline-block;font-size: 14px;margin: 1px px;border-radius: 34px;font-weight: 700;width: 85px;">
+                            Products
+                        </button>
+                    </div>
+                    <div class="col-md-1" style="padding-right: 8.4rem;">
+                        <button style="/* background-color: lightslategray; */border: none;color: dimgray;padding: 11px;text-align: center;/* text-decoration: none; */display: inline-block;font-size: 14px;margin: 1px px;border-radius: 34px;font-weight: 700;width: 100px;">
+                            For Teams
+                        </button>
+                    </div>
+                    <div class="col-md-5">
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
+                            <button type="submit" style="border: none;color: dimgray;padding: 6px;text-align: center;/* text-decoration: none; */display: inline-block;border-radius: 34px;width: 50px;">
+                                <span class="material-icons">search</span></button>
+                        </form>
+                    </div>
+                    <div class="col-md-auto" style="padding-left: 2rem;">
+                        <button type="button" class="btn btn-outline-primary" style="background-color: #DEECF6;">Log in</button>
+                    </div>
+                    <div class="col-md-auto" style="padding-left: 0.2rem;">
+                        <button type="button" class="btn btn-primary" style="background-color: #52A3DC;">Sign up</button>
+                    </div>
+                </div>
             </nav>
         </div>
         <div class="row">
             <div class="col-12">
                 <a href="index.php">
-                    
+
                 </a>
 
             </div>
