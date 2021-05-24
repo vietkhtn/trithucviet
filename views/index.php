337cd35c871d9e7601e8d12e2b1f08f1b207c43c
PhamTrung99 <teotu22@gmail.com>
2021-05-24 16:50:59 +0700

    update sidebars

--- views/index.php
+++ views/index.php
@@ -54,67 +54,42 @@
             </nav>
         </div>
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-2" style="padding: 0rem;">
                 <!--======================== sidebar====================================================================== -->
-                <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
-                    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-                        <svg class="bi me-2" width="30" height="24">
-                            <use xlink:href="#bootstrap"></use>
-                        </svg>
-                        <span class="fs-5 fw-semibold">Collapsible</span>
-                    </a>
+                <div class="flex-shrink-0 p-3 bg-white" style="width: 200px;">
+
                     <ul class="list-unstyled ps-0">
                         <li class="mb-1">
-                            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                                Home
+                        <button class="btn btn-toggle align-items-center rounded collapsed">
+                                HOME
                             </button>
-                            <div class="collapse show" id="home-collapse">
+                        </li>
+                        <li class="mb-1">
+                            <button class="btn btn-toggle align-items-center rounded collapsed">
+                                PUBLIC
+                            </button>
+                            <div>
                                 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                    <li><a href="#" class="link-dark rounded">Overview</a></li>
-                                    <li><a href="#" class="link-dark rounded">Updates</a></li>
-                                    <li><a href="#" class="link-dark rounded">Reports</a></li>
+                                    <li><a href="#" class="link-dark rounded">Questions</a></li>
+                                    <li><a href="#" class="link-dark rounded">Tags</a></li>
+                                    <li><a href="#" class="link-dark rounded">User</a></li>
                                 </ul>
                             </div>
                         </li>
                         <li class="mb-1">
-                            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                                Dashboard
+                            <button class="btn btn-toggle align-items-center rounded collapsed">
+                                FIND A JOBS
                             </button>
-                            <div class="collapse" id="dashboard-collapse">
+                            <div>
                                 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                    <li><a href="#" class="link-dark rounded">Overview</a></li>
-                                    <li><a href="#" class="link-dark rounded">Weekly</a></li>
-                                    <li><a href="#" class="link-dark rounded">Monthly</a></li>
-                                    <li><a href="#" class="link-dark rounded">Annually</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="mb-1">
-                            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                                Orders
-                            </button>
-                            <div class="collapse" id="orders-collapse">
-                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                    <li><a href="#" class="link-dark rounded">New</a></li>
-                                    <li><a href="#" class="link-dark rounded">Processed</a></li>
-                                    <li><a href="#" class="link-dark rounded">Shipped</a></li>
-                                    <li><a href="#" class="link-dark rounded">Returned</a></li>
+                                    <li><a href="#" class="link-dark rounded">Jobs</a></li>
+                                    <li><a href="#" class="link-dark rounded">Companies</a></li>
                                 </ul>
                             </div>
                         </li>
                         <li class="border-top my-3"></li>
                         <li class="mb-1">
-                            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                                Account
-                            </button>
-                            <div class="collapse" id="account-collapse">
-                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                    <li><a href="#" class="link-dark rounded">New...</a></li>
-                                    <li><a href="#" class="link-dark rounded">Profile</a></li>
-                                    <li><a href="#" class="link-dark rounded">Settings</a></li>
-                                    <li><a href="#" class="link-dark rounded">Sign out</a></li>
-                                </ul>
-                            </div>
+                            <div style="font-size: small;"><b>Stack Overflow for Teams </b> – Collaborate and share knowledge with a private group</div>
                         </li>
                     </ul>
                 </div>
