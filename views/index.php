098a1ee306957760190cc0462341790df77db579
PhamTrung99 <teotu22@gmail.com>
2021-05-26 16:30:20 +0700

    update index.php

--- views/index.php
+++ views/index.php
@@ -27,13 +27,15 @@
             <div class="col-md-2" style="border-right-style: inset;border-width: 2px;"></div>
             <div class="col-md-10" style="padding-top: 56px;">
                 <?php include 'partials/headContent.php'; ?>
-             
+
                 <?php include 'partials/post.php'; ?>
 
             </div>
 
         </div>
+
     </div>
+    <?php include 'partials/footer.php'; ?>
     <script src="../assets/bootstrap/js/bootstrap.bundle.js"></script>
     <script src="../assets/bootstrap/js/popper.js"></script>
     <script src="../assets/bootstrap/js/sidebars.js"></script>
---
+++ views/partials/footer.php
@@ -0,0 +1,17 @@
+<div class="row" style="background-color: #242729;padding-top: 20px;padding-bottom: 100px">
+    <div class="col-md-1">
+        <img src="../assets/image/intro.png" alt="">
+    </div>
+    <div class="col-md-3 " style="font-weight: 700;padding-top: 14px;">
+        <div class="div" style="color: #BBC0C4;font-size: 16px;">STACK OVERFLOW</div>
+    </div>
+    <div class="col-md-2 " style="font-weight: 700;padding-top: 14px;">
+        <div class="div" style="color: #BBC0C4;font-size: 16px;">PRODUCTS</div>
+    </div>
+    <div class="col-md-2 " style="font-weight: 700;padding-top: 14px;">
+        <div class="div" style="color: #BBC0C4;font-size: 16px;">COMPANY</div>
+    </div>
+    <div class="col-md-3 " style="font-weight: 700;padding-top: 14px;">
+        <div class="div" style="color: #BBC0C4;font-size: 16px;">STACK EXCHANGE NETWORK</div>
+    </div>
+</div>
