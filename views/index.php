5b8a69c1bb5d98432a1510b0d49550438bba37b4
PhamTrung99 <teotu22@gmail.com>
2021-05-26 14:11:52 +0700

    update index.php

--- views/index.php
+++ views/index.php
@@ -26,55 +26,7 @@
             </div>
             <div class="col-md-2" style="border-right-style: groove;"></div>
             <div class="col-md-10" style="padding-top: 56px;">
-                <div class="row" style="height: 90px; align-items: center;">
-                    <div class="col-md-10">
-                        <h2 style="font-weight: 400;">All Questions</h2>
-                    </div>
-                    <div class="col-md-2" style="text-align: end;">
-                        <div class="col-md-auto" style="padding-left: 0.2rem;">
-                            <button type="button" class="btn btn-primary" style="background-color: #52A3DC;">Ask
-                                Question</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="row" style="border-bottom-style: groove;padding-bottom: 10px;">
-                    <div class="col-md-5">
-                        <div style="font-family: cursive; font-size: 24px;">342 questions with bounties</div>
-                    </div>
-                    <div class="col-md-6" style="text-align: right;">
-                        <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-outline-secondary">Newest</button>
-                            <button type="button" class="btn btn-outline-secondary">Active</button>
-                            <button type="button" class="btn btn-outline-secondary" style="display: inline;">Bountied
-                                <div class="badge bg-primary text-wrap"
-                                    style="width: 2.2rem;height: 1.1rem;padding: 0.2em 0.2em;">
-                                    349
-                                </div>
-                            </button>
-                            <button type="button" class="btn btn-outline-secondary">Unanswered</button>
-                            <button id="btnGroupDrop1" type="button" class="btn  btn-outline-secondary dropdown-toggle"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                More
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                <li><a class="dropdown-item" href="#">Frequent</a></li>
-                                <li><a class="dropdown-item" href="#">Votes</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-1">
-
-                        <button type="button" class="btn btn-outline-secondary"
-                            style="background-color: #DDEBF5;color: cornflowerblue; height: 40px; width:84px">
-                            <span class="material-icons" style="font-size: 18px;">
-                                settings
-                            </span>
-                            Filter
-                        </button>
-                    </div>
-
-                </div>
-
+                <?php include 'partials/headContent.php'; ?>
                 <div class="row" style="padding-bottom: 2rem;padding-top: 0.9rem;">
                     <div class="col-md-1" style="text-align: center;font-family: cursive;">
                         <div>
@@ -107,35 +59,55 @@
                             launching a new ...
                         </div>
                         <div class="row">
-                            <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
-                                <div
-                                    style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 8px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
-                                    <span class="material-icons material-icons-outlined">
-                                        sell
-                                    </span>Tags
+                            <div class="col-md-8" style="display: inline-flex;">
+                                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                                    <div
+                                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 8px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                                        <span class="material-icons material-icons-outlined">
+                                            sell
+                                        </span>Tags
+                                    </div>
+                                </div>
+                                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                                    <div
+                                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                                        Android
+                                    </div>
+                                </div>
+                                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                                    <div
+                                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                                        Youtube
+                                    </div>
+                                </div>
+                                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                                    <div
+                                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                                        ios
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
-                                <div
-                                    style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
-                                    Android
-                                </div>
-                            </div>
-                            <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
-                                <div
-                                    style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
-                                    Youtube
-                                </div>
-                            </div>
-                            <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
-                                <div
-                                    style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
-                                    ios
+                            <div class="col-md-4">
+                                <div class="row">asked May 17 at 5:22</div>
+                                <div class="row">
+                                    <div class="col-md-auto" style="padding-left: 0;">
+                                        <img src="../assets/image/HaiLe.jpg" alt="HaiLe"
+                                            style="height: 38px;width: 38px;padding-left: 0px;">
+                                    </div>
+                                    <div class="col-md-auto">
+                                        <div class="row">Hải Tú</div>
+                                        <div class="row" style="display: inline-block;">
+                                        269
+                                        <span></span>1
+                                        <span></span>2
+                                        <span></span>3
+                                        </div>
+                                        
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                        </div>
+
                     </div>
                 </div>
 
