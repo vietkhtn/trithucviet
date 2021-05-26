2c191cbf371fc7b65c74510457f39299dcaa2f3b
PhamTrung99 <teotu22@gmail.com>
2021-05-26 15:15:22 +0700

    update index.php

---
+++ views/partials/post.php
@@ -0,0 +1,82 @@
+<?php for($i = 0; $i< 10; $i++){?>
+<div class="row" style="padding-bottom: 2rem;padding-top: 0.9rem;">
+    <div class="col-md-1" style="text-align: center;font-family: cursive;">
+        <div>
+            <div class="col-md-row">1</div>
+            <div class="col-md-row" style="padding-bottom: 6px;">votes</div>
+        </div>
+
+        <div style="border-color: #5EBA7D;border-style: solid;border-radius: 5px;border-width: 1.8px;">
+            <div class="col-md-row">2</div>
+            <div class="col-md-row">answers</div>
+        </div>
+        <!-- <div style="border-color: #5EBA7D;border-style: solid;border-radius: 5px;border-width: 1.8px;color: white;font-weight: 600;background-color: #5EBA7D;">
+                            <div class="col-md-row">2</div>
+                            <div class="col-md-row">answers</div>
+                        </div> -->
+        <div class="col-md-row" style="padding-top: 10px;">702 views</div>
+    </div>
+    <div class="col-md-11">
+        <div class="row">
+            <div class="badge bg-primary text-wrap"
+                style="width: 2.2rem;height: 1.1rem;padding: 0.2em 0.2em;font-weight: 500;margin-top: 7px;">
+                +50
+            </div>
+            <div class="col-md-auto"><a href="" style="text-decoration: none;font-size: 22px;">How to
+                    open Youtube in android ?</a></div>
+        </div>
+        <div class="row" style="font-size: 16px;padding-bottom: 16px;padding-top: 2px;">
+            I'm running the latest version of Selenium WebDriver with Geckodriver. I want to prevent
+            Selenium from creating temporary Firefox Profiles in the temporary files directory when
+            launching a new ...
+        </div>
+        <div class="row">
+            <div class="col-md-9" style="display: inline-flex;padding-top: 23px;">
+                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                    <div
+                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 8px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                        <span class="material-icons material-icons-outlined">
+                            sell
+                        </span>Tags
+                    </div>
+                </div>
+                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                    <div
+                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                        Android
+                    </div>
+                </div>
+                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                    <div
+                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                        Youtube
+                    </div>
+                </div>
+                <div class="col-md-auto" style="padding-left: 0;padding-right: 0.4rem;">
+                    <div
+                        style="flex-wrap: wrap;display: flex;background-color: #D1E5F1;font-family: cursive;font-size: 16px;padding-left: 7px;padding-right: 7px;padding-top: 2px;padding-bottom: 3px;border-radius: 7px;">
+                        ios
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3" style="padding-left: 53px;">
+                <div class="row">asked May 17 at 5:22</div>
+                <div class="row">
+                    <div class="col-md-auto" style="padding-left: 0;">
+                        <img src="../assets/image/HaiLe.jpg" alt="HaiLe"
+                            style="height: 38px;width: 38px;padding-left: 0px;">
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="row" style="font-weight: 600;color: #0095FF;">Hải Tú</div>
+                        <div class="row" style="display: inline-block;color: #848D95;font-size: 14px;font-weight: 700;">
+                            269
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?php } ?>
