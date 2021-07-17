ea8ca5004e41f8357e6fbfb31eebe9e57b3c8439
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-07-17 14:52:16 +0700

    updagte

--- README.md
+++ README.md
@@ -4,12 +4,6 @@
 3. Acc: bachviet@gmail.com
 4. Pass: 123456
 
-## Info Mysql Host
-1. DB_NAME="bitgqltbcw1nl6jid2u2"
-2. HOST="bitgqltbcw1nl6jid2u2-mysql.services.clever-cloud.com"
-3. USER="uboe6mrf9gqq6mvo"
-4. PASSWORD="LZ55sk6B8o7IjVP5DyYM"
-5. PORT="3306"
 
 ## Info API check bad word
 - URL: [checkapibadword](https://checkbadwordapi.herokuapp.com/)
--- core/database/connection.php
+++ core/database/connection.php
@@ -1,7 +1,11 @@
 <?php 
- $hostInfo = 'mysql:host=bjhmz1rb32kuq60eo0v4-mysql.services.clever-cloud.com;dbname=bjhmz1rb32kuq60eo0v4;charset=utf8mb4';
- $userAdmin = 'uwajkb81wg9v9z6b';
- $passAdmin = 'mp42r1Xetr3wuumJVeCj';
+//  $hostInfo = 'mysql:host=bjhmz1rb32kuq60eo0v4-mysql.services.clever-cloud.com;dbname=bjhmz1rb32kuq60eo0v4;charset=utf8mb4';
+//  $userAdmin = 'uwajkb81wg9v9z6b';
+//  $passAdmin = 'mp42r1Xetr3wuumJVeCj';
+$hostInfo = 'mysql:host=127.0.0.1:3306;dbname=stackoverflow;charset=utf8mb4';
+$userAdmin = 'root';
+$passAdmin = 'Teotu_19';
+
 try {
     $pdo = new PDO($hostInfo, $userAdmin, $passAdmin);
     //echo "Connected successfully";
