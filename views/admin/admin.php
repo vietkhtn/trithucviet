b1791998a69e6dcd17d96cfbc022b98efdbca859
phamvantrinh99 <trinhpham5337@gmail.com>
2021-07-18 10:36:29 +0700

    update admin

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -1,3 +1,5 @@
+<?php require '../../controllers/adminController.php' ?>
+
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
@@ -58,53 +60,20 @@
                                             <th class="border-top-0">#</th>
                                             <th class="border-top-0">First Name</th>
                                             <th class="border-top-0">Last Name</th>
-                                            <th class="border-top-0">Username</th>
-                                            <th class="border-top-0">Role</th>
+                                            <th class="border-top-0">Email</th>
+                                            <th class="border-top-0">BirthDay</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Deshmukh</td>
-                                            <td>Prohaska</td>
-                                            <td>@Genelia</td>
-                                            <td>admin</td>
+                                        <?php forEach ($usersList as $user) { ?>
+                                            <tr>
+                                            <td><?php echo($user->user_id) ?></td>
+                                            <td><?php echo($user->first_name) ?></td>
+                                            <td><?php echo($user->last_name) ?></td>
+                                            <td><?php echo($user->email) ?></td>
+                                            <td><?php echo($user->birthday) ?></td>
                                         </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Deshmukh</td>
-                                            <td>Gaylord</td>
-                                            <td>@Ritesh</td>
-                                            <td>member</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Sanghani</td>
-                                            <td>Gusikowski</td>
-                                            <td>@Govinda</td>
-                                            <td>developer</td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td>Roshan</td>
-                                            <td>Rogahn</td>
-                                            <td>@Hritik</td>
-                                            <td>supporter</td>
-                                        </tr>
-                                        <tr>
-                                            <td>5</td>
-                                            <td>Joshi</td>
-                                            <td>Hickle</td>
-                                            <td>@Maruti</td>
-                                            <td>member</td>
-                                        </tr>
-                                        <tr>
-                                            <td>6</td>
-                                            <td>Nigam</td>
-                                            <td>Eichmann</td>
-                                            <td>@Sonu</td>
-                                            <td>supporter</td>
-                                        </tr>
+                                        <?php } ?>
                                     </tbody>
                                 </table>
                             </div>
