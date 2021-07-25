9c6607d08be8c210bcc11855f081d9c2a3d051a6
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-07-25 10:51:58 +0700

    update

--- views/userranking.php
+++ views/userranking.php
@@ -24,19 +24,10 @@
             <!-- ============================================================== -->
             <div class="page-breadcrumb bg-white">
                 <div class="row align-items-center">
+                    <h4 class="page-title">User Ranking</h4>
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Manage Answer</h4>
                     </div>
-                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                        <div class="d-md-flex">
-                            <ol class="breadcrumb ms-auto">
-                                <li><a href="#" class="fw-normal">Dashboard</a></li>
-                            </ol>
-                            <a href="" target="_blank"
-                                class="btn btn-danger  d-none d-md-block pull-right ms-3 hidden-xs hidden-sm waves-effect waves-light text-white">Upgrade
-                                to Pro</a>
-                        </div>
-                    </div>
+
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -51,10 +42,19 @@
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
                 <div class="row">
-                    <div class="col-sm-12">
-                        <div class="white-box">
-                            <div>
-                                <canvas id="myChart"></canvas>
+                    <div class="col-sm-12" style="display: -webkit-inline-box;">
+                        <div class="col-sm-6">
+                            <div class="white-box">
+                                <div>
+                                    <canvas id="userScoreChart"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="white-box">
+                                <div>
+                                    <canvas id="userTTQuesChart"></canvas>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -91,63 +91,53 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.4.1/dist/chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'October',
-    ];
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: 'Top Ranking',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
-            ],
-            borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)'
-            ],
-            borderWidth: 1
-        }]
-    };
-    const config = {
-        type: 'bar',
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    const dataUserRank = JSON.parse('<?=json_encode($userRank)?>')
+    function configChartBar(result, labelChart, labelItemName, dataItemName, backGroundColor, borderColor) {
+        let labels = [];
+        let datas = [];
+        result.map(item => {
+            labels.push(item[`${labelItemName}`])
+            datas.push(item[`${dataItemName}`])
+        })
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: labelChart,
+                data: datas,
+                backgroundColor: backGroundColor,
+                borderColor: borderColor,
+                borderWidth: 1
+            }]
+        };
+        return {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        },
-    };
+        };
+    }
 
-    var myChart = new Chart(
-        document.getElementById('myChart'),
-        config
+    const configUserScoreChart = configChartBar(dataUserRank,"Score","screen_name", "total", 'rgba(255, 159, 64, 0.2)','rgb(255, 159, 64)');
+    const configUserTotalQues = configChartBar(dataUserRank,"Total Question","screen_name", "total_question", 'rgba(54, 162, 235, 0.2)','rgb(54, 162, 235)');
+
+    var userScoreChart = new Chart(
+        document.getElementById('userScoreChart'),
+        configUserScoreChart
+    );
+    var userTTQuesChart = new Chart(
+        document.getElementById('userTTQuesChart'),
+        configUserTotalQues
     );
     </script>
 
     <script>
     $(".tab-manage-user-ranking").addClass('active');
     </script>
-
 </body>
 
 </html>
